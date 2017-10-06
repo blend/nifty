f18db0ddf39d66700365386895517a94a029c674
Will Charczuk <will@blendlabs.com>
2017-10-06 13:52:22 -0700

    JAVASCRIPT

--- src/connection.ts
+++ src/connection.ts
@@ -1,18 +1,33 @@
-import { Pool, Client } from 'pg';
+import { Pool, Client } from "pg";
 
 // Connection represents the metadata used to make the initial conneciton.
 class Connection {
-	Host: string;
-	Port: string;
-	Database: string;
-	Schema: string;
-	Username: string;
-	Password: string;
-	SSLMode: string;
+  Host: string;
+  Port: string;
+  Database: string;
+  Schema: string;
+  Username: string;
+  Password: string;
+  SSLMode: string;
 
-	Pool: Pool;
+  Pool: Pool;
 
-	public Close() {
+  // Open either returns the current pool or creates a new pool.
+  public Open() {}
 
-	}
+  // OpenNew creates the pool and opens the initial connection.
+  public OpenNew() {}
+
+  // Prepare creates and returns a postgres query plan for a given statement.
+  // You can optionally cache this plan for re-use.
+  public Prepare(statement: string) {}
+
+  // Begin should start a transaction.
+  public Begin() {}
+
+  // Close destroys the pool and underlying connections.
+  public Close() {}
+
+  // Run returns a new context.
+  public Run() {}
 }
