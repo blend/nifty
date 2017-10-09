eb5bc358bfe0c7a6bf1b52e3367b9000bfff1040
Will Charczuk <will@blendlabs.com>
2017-10-09 12:27:46 -0700

    get works

--- examples/main.ts
+++ examples/main.ts
@@ -32,6 +32,7 @@ async function main() {
 	console.log("creating object complete");
 	console.log("created", md, md.ID)
 
+	// this seems anus tier but is necessary because typescript is not a real language.
 	let verify = await conn.Get<MetadataTest>(MetadataTest, md.ID)
 	console.log(verify)
 
