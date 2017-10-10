9cb3efe3befcb1e8007536e366207ee4d3fa1f97
Arjun Baokar <arjunbaokar@gmail.com>
2017-10-09 18:38:48 -0700

    ava not detecting test files

--- dist/types/src/invocation.d.ts
+++ dist/types/src/invocation.d.ts
@@ -1,28 +1,28 @@
 import { Client } from "pg";
 import { Query } from "./query";
 export interface InvocationConfig {
-    Client: Client;
+    client: Client;
 }
 export declare class Invocation {
-    Connection: Client;
-    Exec(statement: string, ...args: any[]): Promise<Error | null>;
-    Query(statement: string, ...args: any[]): Promise<Query>;
-    Begin(): Promise<Error | null>;
-    Commit(): Promise<Error | null>;
-    Rollback(): Promise<Error | null>;
-    Close(): Promise<Error | null>;
-    Get<T>(typeDef: {
+    connection: Client;
+    exec(statement: string, ...args: any[]): Promise<Error | null>;
+    query(statement: string, ...args: any[]): Promise<Query>;
+    begin(): Promise<Error | null>;
+    commit(): Promise<Error | null>;
+    rollback(): Promise<Error | null>;
+    close(): Promise<Error | null>;
+    get<T>(typeDef: {
         new (): T;
     }, ...ids: any[]): Promise<T | Error>;
-    GetAll<T>(typeDef: {
+    getAll<T>(typeDef: {
         new (): T;
     }): Promise<Array<T> | Error>;
-    Create(obj: any): Promise<Error | null>;
-    CreateMany(...objs: any[]): Promise<Error | null>;
-    Update<T>(obj: T): Promise<Error | null>;
-    Upsert<T>(obj: T): Promise<Error | null>;
-    Delete(obj: any): Promise<any>;
-    Truncate<T>(typeDef: {
+    create(obj: any): Promise<Error | null>;
+    createMany(...objs: any[]): Promise<Error | null>;
+    update<T>(obj: T): Promise<Error | null>;
+    upsert<T>(obj: T): Promise<Error | null>;
+    delete(obj: any): Promise<any>;
+    truncate<T>(typeDef: {
         new (): T;
     }): Promise<Error | null>;
 }
--- examples/main.ts
+++ examples/main.ts
@@ -13,27 +13,27 @@ class MetadataTest {
 
 async function migrate(conn: Connection) {
 	console.log("creating metadata table");
-	let result = await conn.Exec(
-		"CREATE TABLE IF NOT EXISTS metadata_test (id serial not null, name varchar(255))"
+	let result = await conn.exec(
+		'CREATE TABLE IF NOT EXISTS metadata_test (id serial not null, name varchar(255))'
 	);
 	console.log("creating metadata table complete");
 
 }
 
 async function main() {
-	var conn = new Connection();
-	await conn.Open();
+	const conn = new Connection();
+	await conn.open();
 	await migrate(conn);
 
 	console.log("creating object");
 	let md = new MetadataTest();
 	md.Name = "foo"
-	await conn.Create(md);
+	await conn.create(md);
 	console.log("creating object complete");
 	console.log("created", md, md.ID)
 
 	// this seems anus tier but is necessary because typescript is not a real language.
-	let verify = await conn.Get<MetadataTest>(MetadataTest, md.ID)
+	let verify = await conn.get<MetadataTest>(MetadataTest, md.ID)
 	console.log(verify)
 
 	return
