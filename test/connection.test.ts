c4a0325610d8d488ba230a6f10a9192c428edffe
Arjun Baokar <arjunbaokar@gmail.com>
2017-10-11 11:27:40 -0700

    tests actually test things now

--- test/connection.test.ts
+++ test/connection.test.ts
@@ -1,6 +1,16 @@
 import test from 'ava';
 import { Connection } from '../src/connection';
+import { Table, Column } from '../src/decorators';
 import { connectionConfig } from './testConfig';
+
+@Table('test')
+class TestData {
+  @Column('id', { PrimaryKey: true, Serial: true })
+  id: number;
+
+  @Column('name')
+  name: string;
+}
 
 test('can open connection', async t => {
   const conn = new Connection();
@@ -9,17 +19,31 @@ test('can open connection', async t => {
 });
 
 test('invoke creates a connection', async t => {
-  const conn = new Connection();
+  const conn = new Connection(connectionConfig);
   conn.open();
   const invocation = await conn.invoke();
   t.not(invocation.connection, undefined);
   await invocation.close();
 });
 
-test('exec can execute a query', async t => {
-  const conn = new Connection();
-  let result = await conn.exec(
-    'CREATE TABLE IF NOT EXISTS test (id serial not null, name varchar(255))'
-  );
-  console.log('\n\nresult ', JSON.stringify(result, null, 2));
+test('exec can execute basic queries', async t => {
+  const conn = new Connection(connectionConfig);
+  conn.open();
+
+  const createResult = await conn.exec('CREATE TABLE IF NOT EXISTS test (id serial not null, name varchar(255))');
+  t.is(createResult, null);
+
+  const insertResult = await conn.exec(`INSERT INTO test (name) VALUES ('testvalue')`);
+  t.is(insertResult, null);
+
+  const selectResult = await conn.exec('SELECT * FROM test');
+  t.is(selectResult, null);
+});
+
+test('exec returns an error on failed queries', async t => {
+  const conn = new Connection(connectionConfig);
+  conn.open();
+
+  const err = await conn.exec('DROP TABLE doesnt_exist');
+  t.truthy(err);
 });
