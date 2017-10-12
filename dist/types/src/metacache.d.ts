60b36dae8a620f914e4498fe933cb9dbe05fc493
Arjun Baokar <arjunbaokar@gmail.com>
2017-10-12 11:34:25 -0700

    docker containers should always delete, even on test failures

--- dist/types/src/metacache.d.ts
+++ dist/types/src/metacache.d.ts
@@ -1,6 +1,6 @@
 import { Columns } from './columns';
 import { ColumnInfo } from './column_info';
-export declare function AddModel(className: string, tableName: string): void;
-export declare function AddModelColumn(className: string, column: ColumnInfo): void;
-export declare function TableNameFor(className: string): string;
-export declare function ColumnsFor(className: string): Columns;
+export declare function addModel(className: string, tableName: string): void;
+export declare function addModelColumn(className: string, column: ColumnInfo): void;
+export declare function tableNameFor(className: string): string;
+export declare function columnsFor(className: string): Columns;
--- docker/test.sh
+++ docker/test.sh
@@ -1,2 +1,9 @@
+function delete_container {
+  docker rm -f test-postgres || true
+}
+
+trap delete_container EXIT
 docker rm -f test-postgres || true
 docker run -d -p 5431:5432 --name test-postgres -e POSTGRES_USER=testuser -e POSTGRES_PASSWORD=testpassword postgres:9.6.3
+npm run buildClean
+NODE_ENV=test nyc --check-coverage -a ava
