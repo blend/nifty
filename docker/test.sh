eeefbdaf393f53f8d719f53233ff29afdeb4b3b0
lmaokar <btacju@gmail.com>
2017-10-16 11:48:43 -0700

    quieted docker error

--- docker/test.sh
+++ docker/test.sh
@@ -3,7 +3,12 @@ function delete_container {
 }
 
 trap delete_container EXIT
-docker rm -f test-postgres || true
+CONTAINER_NAME='test-postgres'
+CID=$(docker ps -q -f status=running -f name=^/${CONTAINER_NAME}$) 
+if [ "${CID}" ]; then
+  docker rm -f test-postgres
+fi
+unset CID
 docker run -d -p 5431:5432 --name test-postgres -e POSTGRES_USER=testuser -e POSTGRES_PASSWORD=testpassword postgres:9.6.3
 npm run buildClean
 NODE_ENV=test nyc --check-coverage -a ava "$@"
