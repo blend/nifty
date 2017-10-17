a43bfdbc4b6128b46d2988dcb0ea17d579d5251b
Arjun Baokar <arjunbaokar@gmail.com>
2017-10-17 15:58:56 -0700

    adding index

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import * as columnInfo from './column_info';
+import * as columns from './columns';
+import * as connection from './connection';
+import * as decorators from './decorators';
+import * as invocation from './invocation';
+import * as metacache from './metacache';
+import * as query from './query';
+import * as interfaces from './interfaces';
+
+export {
+  columnInfo,
+  columns,
+  connection,
+  decorators,
+  invocation,
+  metacache,
+  query,
+  interfaces
+}
